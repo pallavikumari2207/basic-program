10db939608a53a0079565c77885def6810d5cb6a
Sayyada29 <prasadpalkar94@gmail.com>
2020-01-13 15:07:49 +0530

    [pallavi].flipcoin

--- fs_prgm/src/main/java/com/bridgelabz/basicprgm/flipcoin.java
+++ fs_prgm/src/main/java/com/bridgelabz/basicprgm/flipcoin.java
@@ -6,36 +6,32 @@ import java.util.Scanner;
 public class flipcoin {
 	public static void main(String[] args) {
 		System.out.println("enter number of times to flip coin");
-		Scanner scan=new Scanner(System.in);
-		int n=scan.nextInt();
-		Random rand=new Random();
+		Scanner scan = new Scanner(System.in);
+		int n = scan.nextInt();
+		Random rand = new Random();
 		prcent(n);
 	}
-	public static void prcent(int n)
-	{
-		int headcount=0;
-		int tailcount=0;
-		//System.out.println(x);
-		for(int i=0;i<n;i++)
-		{	Random rand=new Random();
-		float x=rand.nextFloat();
-		System.out.println(x);
-		if(x<0.5)
-		{
-		headcount++;
-		}
-		else
-		{
-		tailcount++;
-		}
+
+	public static int prcent(int n) {
+		int headcount = 0;
+		int tailcount = 0;
+		// System.out.println(x);
+		for (int i = 0; i < n; i++) {
+			Random rand = new Random();
+			float x = rand.nextFloat();
+			System.out.println(x);
+			if (x < 0.5) {
+				headcount++;
+			} else {
+				tailcount++;
+			}
 		}
 		System.out.println(headcount);
 		System.out.println(tailcount);
-		float percent_head=(((float)(headcount)/(float)n)*100);
+		float percent_head = (((float) (headcount) / (float) n) * 100);
 		System.out.println(percent_head);
-		float percent_tail=(((float)(tailcount)/(float)n)*100);
+		float percent_tail = (((float) (tailcount) / (float) n) * 100);
 		System.out.println(percent_tail);
-		}
+		return (int) percent_head;
 	}
-		
-
+}
