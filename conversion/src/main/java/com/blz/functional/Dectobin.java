3c749a272909959003727c5a3f3beecd0e58dc04
Sayyada29 <prasadpalkar94@gmail.com>
2020-01-22 12:17:54 +0530

    [pallavi].decimalToBinaryConversion

--- conversion/src/main/java/com/blz/functional/Dectobin.java
+++ conversion/src/main/java/com/blz/functional/Dectobin.java
@@ -1,8 +1,30 @@
 package com.blz.functional;
 
+import java.util.Scanner;
+
 public class Dectobin {
 public static void main(String[] ags)
+{   int r=0;
+	int [] rem=new int[32];
+Scanner scan= new Scanner(System.in);
+System.out.println("enter the number");
+int n= scan.nextInt();
+//conversion
+	while(n!=0)
+	{
+		for(int i=0;i<rem.length;i++)
+	{  
+		r=n%2;
+		n=n/2;
+		rem[i]=r;
+		r=0;
+	}
+
+}
+//for binary printing 
+for(int i=rem.length-1;i>=0;i--)
 {
-	
+System.out.print(rem[i]);	
+}
 }
 }
