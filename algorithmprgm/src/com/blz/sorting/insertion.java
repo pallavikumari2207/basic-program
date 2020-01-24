d0071c2d5e026c65436b3aaea4cf831ad847c5a9
Sayyada29 <prasadpalkar94@gmail.com>
2020-01-24 10:17:39 +0530

    [pallavi].insertion

---
+++ algorithmprgm/src/com/blz/sorting/insertion.java
@@ -0,0 +1,31 @@
+package com.blz.sorting;
+
+public class insertion {
+public static void main(String []args)
+{
+	int [] arr= {7,5,4,3,6,8};
+	System.out.println("unsorted array is:");
+	for (int i=0;i<arr.length;i++)
+	{
+		System.out.print(arr[i]);	
+	}
+	System.out.println();
+	for(int i=1;i<arr.length;i++)
+	{	int key=arr[i];
+		int j=i-1;
+	  while (j >= 0 && arr[j] > key) 
+	{
+		arr[j+1]=arr[j];
+		j=j-1;
+	}
+	  arr[j+1]=key;
+	  
+}
+	System.out.println("sorted array via:insertion");
+for(int k=0;k<arr.length;k++)
+{
+
+System.out.print(arr[k]);	
+}
+}
+}
